fd56dbca89fec1c3d14c76460b4342a6ef5aa495
Aarit Jayaswal <aaritrj@gmail.com>
2024-09-07 13:01:18 -0700

    Almost finished with movement, working on smoother movement

--- src/game/GamePanel.java
+++ src/game/GamePanel.java
@@ -13,12 +13,12 @@ import javax.swing.Timer;
 public class GamePanel extends JPanel implements ActionListener, KeyListener{
 	Player player;
 	Timer timer;
-	
+
 	GamePanel(){
 		player = new Player(200,200,70,70);
 		timer = new Timer(1000/60, this);
 		timer.start();
-		
+
 	}
 	public void drawPlayer(Graphics g) {
 		player.draw(g);
@@ -28,67 +28,64 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener{
 		player.update();
 		repaint();		
 	}
-	
-	
+
+
 	@Override
 	protected void paintComponent(Graphics g) {
 		g.setColor(Color.BLACK);
 		g.fillRect(0, 0, Game.WIDTH, Game.HEIGHT);
 		drawPlayer(g);
-		
+
 	}
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 	@Override
 	public void keyPressed(KeyEvent e) {
 		if(e.getKeyCode()==KeyEvent.VK_UP || e.getKeyCode()==KeyEvent.VK_SPACE) {
-			player.direction = 0;
 			player.isJumping = true;
-			
+			player.yspeed = -20;
+
 		}
-		
+
 		else if(e.getKeyCode()==KeyEvent.VK_DOWN) {
-			player.direction = 2;
 			player.isFalling = true;
 		}
-		
+
 		else if(e.getKeyCode()==KeyEvent.VK_LEFT) {
 			player.direction = 1;
+			player.isLeft = true;
 			player.isWalking = true;
 		}
-		
+
 		else if(e.getKeyCode()==KeyEvent.VK_RIGHT) {
-			player.direction = 3;
+			player.direction = 2;
+			player.isLeft = false;
 			player.isWalking = true;
 		}
-		
+
 	}
 	@Override
 	public void keyReleased(KeyEvent e) {
-		if(e.getKeyCode()==KeyEvent.VK_UP || e.getKeyCode()==KeyEvent.VK_SPACE) {
-			player.isJumping = false;
-			player.isFalling = true;
-		}
-		
-		else if(e.getKeyCode()==KeyEvent.VK_DOWN) {
+
+		if(e.getKeyCode()==KeyEvent.VK_DOWN) {
 			player.isFalling = false;
 		}
-		
+
 		else if(e.getKeyCode()==KeyEvent.VK_LEFT) {
 			player.isWalking = false;
 		}
-		
+
 		else if(e.getKeyCode()==KeyEvent.VK_RIGHT) {
 			player.isWalking = false;
 		}
-		
-		
-		
-		
+
+
+
+
 	}
-	
-	
+
+
 }
--- src/game/Player.java
+++ src/game/Player.java
@@ -15,13 +15,19 @@ public class Player extends GameObject {
 	boolean isWalking;
 	BufferedImage[] walkImages;
 	BufferedImage[] jumpImages;
-	int currentImage;
+	BufferedImage[] idleImages;
+	int walkCurrent;
+	int idleCurrent;
 	int direction;
 	boolean keepDirection;
-	int imageNumber = 1;
+	int walkNum = 1;
+	int idleNum = 1;
 	boolean isJumping;
 	boolean isFalling;
 	Image fall;
+	int yspeed;
+	boolean onSurface = false;
+	boolean isLeft;
 
 	public Player(int x, int y, int width, int height) {
 		super(x, y, width, height);
@@ -29,63 +35,130 @@ public class Player extends GameObject {
 		isWalking = false;
 		isJumping = false;
 		isFalling = false;
-		currentImage = 0;
+		walkCurrent = 0;
+		idleCurrent = 0;
 		direction = 0;
 		walkImages = new BufferedImage[36];
 		jumpImages = new BufferedImage[7];
-		
+		idleImages = new BufferedImage[55];
+
+
 		walk();
 		jump();
 		fall();
-		
+		idle();
+
 
 	}
 
 	void draw(Graphics g) {
 
-//		g.setColor(Color.BLUE);
-//		g.fillRect(x, y, width, height);
+		//		g.setColor(Color.BLUE);
+		//		g.fillRect(x, y, width, height);
 
 		if (isWalking == true && direction == 1) {
-			g.drawImage(walkImages[currentImage], x, y, -width, height, null);
-			currentImage += 1;
-			currentImage %= 36;
+			g.drawImage(walkImages[walkCurrent], x, y, -width, height, null);
+			walkCurrent += 1;
+			walkCurrent %= 36;
 			keepDirection = true;
 
-		} else if (isWalking == true && direction == 3) {
-			g.drawImage(walkImages[currentImage], x, y, width, height, null);
-			currentImage += 1;
-			currentImage %= 36;
+		} else if (isWalking == true && direction == 2) {
+			g.drawImage(walkImages[walkCurrent], x, y, width, height, null);
+			walkCurrent += 1;
+			walkCurrent %= 36;
 
-		} else if (isFalling == true && direction == 2) {
+		} else if (isFalling == true && isLeft == false) {
 			g.drawImage(fall, x, y, width, height, null);
 
-		} else if (isJumping == true && direction == 0) {
-			g.drawImage(jumpImages[0], x, y, width, height, null);
-//			currentImage += 1;
-//			currentImage %= 36;
+		} else if (isFalling == true && isLeft) {
+			g.drawImage(fall, x, y, -width, height, null);
 
+		} else if (isJumping == true && isLeft == false) {
+			g.drawImage(jumpImages[0], x, y, width, height, null);
+			//			currentImage += 1;
+			//			currentImage %= 36;
+
+		} else if (isJumping == true && isLeft) {
+			g.drawImage(jumpImages[0], x, y, -width, height, null);
+			//			currentImage += 1;
+			//			currentImage %= 36;
+			
+		if(isJumping == true && isWalking == true && isLeft) {
+			g.drawImage(jumpImages[0], x, y, -width, height, null);
+		}
+		
 		} else {
-			g.drawImage(walkImages[0], x, y, width, height, null);
+
+			if(isLeft) {
+				g.drawImage(idleImages[idleCurrent], x, y, -width, height, null);
+				idleCurrent += 1;
+				idleCurrent %= 55;
+				keepDirection = true;
+			}
+
+			else {
+				g.drawImage(idleImages[idleCurrent], x, y, width, height, null);
+				idleCurrent += 1;
+				idleCurrent %= 55;
+			}
+
 
 		}
 
 	}
 
-	public void up() {
-		y -= 10;
-		if (y <= 0) {
+	//	public void up() {
+	//			yspeed+=5;
+	//			System.out.println(yspeed);
+	//			y-= yspeed;
+	//			if(yspeed <=0) {
+	//				down();
+	//		}
+	//		
+	//		
+	//		
+	//		
+	//		if (y <= 0) {
+	//			y = 0;
+	//		}
+	//		super.update();
+	//	}
+	//
+	//	public void down() {
+	//		for(int i =0; i <6; i++) {
+	//			yspeed +=i;
+	//			System.out.println(yspeed);
+	//			y += yspeed;
+	//		}
+	//		if (y >= 525) {
+	//			y = 525;
+	//		}
+	//		super.update();
+	//	}
+
+	public void jumping() {
+		if(onSurface) {
+			yspeed = 0;
+		}
+		else {
+			yspeed+=1;
+		}
+		y+=yspeed;
+
+		if(yspeed>0) {
+			isFalling = true;
+		}
+
+		if(y>=500) {
+			yspeed = 0;
+			y = 500;
+			isJumping = false;
+			isFalling = false;
+		}
+
+		if(y<=0) {
 			y = 0;
 		}
-		super.update();
-	}
-
-	public void down() {
-		y += 10;
-		if (y >= 525) {
-			y = 525;
-		}
-		super.update();
 	}
 
 	public void left() {
@@ -105,59 +178,86 @@ public class Player extends GameObject {
 	}
 
 	void update() {
-		if (direction == 0 && isJumping == true) {
-			up();
-		} else if (direction == 1 && isWalking == true) {
+		//		if (direction == 0 && isJumping == true) {
+		//			up();} 
+		if (direction == 1 && isWalking == true) {
 			left();
 		}
-		if (direction == 2 && isWalking == true) {
-			down();
-		}
-		if (direction == 3 && isWalking == true) {
+		//		if (direction == 2 && isWalking == true) {
+		//			down();
+		//		}
+		else if (direction == 2 && isWalking == true) {
 			right();
 		}
+
+		if (isJumping == true) {
+			jumping();
+		}
+
 	}
-	
+
+
+
 	void walk() {
-		
+
 		for (int i = 1; i < 37; i++) {
 
 			try {
 				walkImages[i - 1] = ImageIO.read(new File(
-						"src/Pixel Adventure 1/Main Characters/Ninja Frog/Run/Frog (" + imageNumber + ").png"));
+						"src/Pixel Adventure 1/Main Characters/Ninja Frog/Run/Frog (" + walkNum + ").png"));
 
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				System.out
-						.println("src/Pixel Adventure 1/Main Characters/Ninja Frog/Run/Frog (" + imageNumber + ").png");
+				.println("src/Pixel Adventure 1/Main Characters/Ninja Frog/Run/Frog (" + walkNum + ").png");
 				e.printStackTrace();
 			}
-			
+
 			if (i % 3 == 0) {
-				imageNumber += 1;
+				walkNum += 1;
 			}
 		}
 	}
-	
+
 	void jump() {
-		
+
 		try {
 			jumpImages [0] = ImageIO.read(new File ("src/Pixel Adventure 1/Main Characters/Ninja Frog/Jump (32x32).png"));
 		}
-		
+
 		catch (Exception e) {
-			
+
 		}
-		
+
 	}
-	
+
 	void fall() {
 		try {
-			fall = ImageIO.read(new File ("src/Pixel Adventure 1/Main Characters/Ninja Frog/Jump (32x32).png"));
+			fall = ImageIO.read(new File ("src/Pixel Adventure 1/Main Characters/Ninja Frog/Fall (32x32).png"));
 		}
-		
+
 		catch (Exception e) {
-			
+
+		}
+	}
+
+	void idle() {
+
+		for (int i = 1; i < 56; i++) {
+
+			try {
+				idleImages[i - 1] = ImageIO.read(new File(
+						"src/Pixel Adventure 1/Main Characters/Ninja Frog/Idle/Idle (" + idleNum + ").png"));
+
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				System.out.println("src/Pixel Adventure 1/Main Characters/Ninja Frog/Idle/Idle (" + idleNum + ").png");
+				e.printStackTrace();
+			}
+
+			if (i % 5 == 0) {
+				idleNum += 1;
+			}
 		}
 	}
 
